b2fd63f642c1cefd2633ad6a8aac624076219f08
Himanshi Sharma <himanshisharma748@gmail.com>
2025-08-04 15:18:23 +0530

    import line fixed

--- client/src/component/CreateTask.jsx
+++ client/src/component/CreateTask.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import axios from 'axios';
+const API = import.meta.env.VITE_API_URL;
 
 const CreateTask = ({ boardId, onTaskCreated }) => {
   const [title, setTitle] = useState('');
